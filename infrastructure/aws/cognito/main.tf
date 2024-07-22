3dc87914765795fd7af608850d9616fbf9bedec1
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-07-21 21:36:40 -0300

    feat: add cognito dir

--- infrastructure/aws/cognito/main.tf
+++ infrastructure/aws/cognito/main.tf
@@ -82,10 +82,3 @@ resource "aws_cognito_user_pool_client" "main" {
   prevent_user_existence_errors = "ENABLED"
   enable_token_revocation       = true
 }
-
-output "cognito_app_client_id" {
-  value = aws_cognito_user_pool_client.main.id
-}
-output "cognito_user_pool_id" {
-  value = aws_cognito_user_pool.main.id
-}
