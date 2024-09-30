d14e2bae7023f773d5a761081ef39c08d9f6ab7b
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-09-30 19:17:27 -0300

    feat: update ses lambda api

--- modules/aws/api_gateway/ssm.tf
+++ modules/aws/api_gateway/ssm.tf
@@ -3,5 +3,5 @@ resource "aws_ssm_parameter" "cognito_user_pool_client_id" {
 
   name  = "/apigateway/${var.api_gateway_name}/invoke_url"
   type  = "String"
-  value = aws_apigatewayv2_deployment.main.invoke_url
+  value = aws_apigatewayv2_api.main.api_endpoint
 }
